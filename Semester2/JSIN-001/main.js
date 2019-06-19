8a2c69bc5250e0a70b1d48404d6fb60dfa5000f6
guzele <Gisele.Garifullina@gmail.com>
2019-06-19 11:41:28 +0300

    JSIN-001

---
+++ Semester2/JSIN-001/main.js
@@ -0,0 +1,14 @@
+//Author Guzel Garifullina
+let fDeg = 32;
+let rubles = 25000;
+let a = 8;
+let b = 45;
+let c = -18;
+
+let cDeg = (5/9) * (fDeg - 32)
+let dollars = 0.016 * rubles;
+
+let d = b*b - 4 *a *c;
+let x1 = (-b - Math.sqrt(d)) / 2 / a;
+let x2 = (-b + Math.sqrt(d)) / 2 / a;
+
